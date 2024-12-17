5208dbee487d9f3dc192933454493c1f12997b3f
Ahmet Celik <celikahmet351@gmail.com>
2024-12-17 11:41:42 -0500

    Delete Public/Scripts/product.js

--- Public/Scripts/product.js
+++
@@ -1,7 +0,0 @@
-[
-    { "id": 1, "name": "Space Travel", "type": "service", "description": "From:Eart, To:Mars" },
-    { "id": 2, "name": "Humanoid", "type": "product", "description": "Mode: 216" },
-    { "id": 3, "name": "Electric Aircraft", "type": "product", "description": "Model: KA-FA 1500" },
-    { "id": 4, "name": "Experiencer Disk", "type": "product", "description": "Customer chose 1950's add-on" }
-  ]
-  
